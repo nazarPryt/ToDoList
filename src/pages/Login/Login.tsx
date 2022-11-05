54dd1d9c77550f66470fb5c9196793da98ff7e20
nazarPtyt <ockap90@libero.it>
2022-11-05 13:17:35 +0100

    16 add Formik + validation form

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,7 +1,29 @@
 import React from 'react';
 import s from './Login.module.css'
+import * as Yup from 'yup';
+import {useFormik} from "formik";
 
 const Login = () => {
+
+    const formik = useFormik({
+        initialValues: {
+            email: '',
+            password: '',
+            rememberMe: false,
+            captcha: '',
+        },
+        validationSchema: Yup.object({
+            email: Yup.string()
+                .email('Invalid email address')
+                .required('Required'),
+            password: Yup.string()
+                .min(3, 'Must be min 3 characters or more')
+                .required('Required'),
+        }),
+        onSubmit: value => {
+            alert(JSON.stringify(value, null, 2));
+        }
+    })
 
 
     return (
@@ -11,15 +33,22 @@ const Login = () => {
                 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Blanditiis, laboriosam! Lorem ipsum dolor
                     sit amet, consectetur adipisicing elit. Eius eum eveniet excepturi harum in praesentium provident
                     quidem quo quos vel.</p>
-                <form action="" className={s.form}>
-                    <input type="email" placeholder={"Email"} className={s.input}/>
-                    <input type="password" placeholder={"Password"} className={s.input}/>
+                <form className={s.form} onSubmit={formik.handleSubmit}>
+                    <input type="email" placeholder={"Email"} className={s.input} {...formik.getFieldProps('email')}/>
+                    {formik.touched.email && formik.errors.email ? (
+                        <div>{formik.errors.email}</div>
+                    ) : null}
+                    <input type="password" placeholder={"Password"}
+                           className={s.input} {...formik.getFieldProps('password')}/>
+                    {formik.touched.password && formik.errors.password ? (
+                        <div>{formik.errors.password}</div>
+                    ) : null}
                     <div className={s.buttonWrap}>
                         <span>
                             <label htmlFor="rebMe">Remember Me !!!</label>
-                            <input type="checkbox" id='rebMe'/>
+                            <input type="checkbox" id='rebMe' {...formik.getFieldProps('rememberMe')}/>
                         </span>
-                        <button className={s.button}>Log in</button>
+                        <button className={s.button} type="submit">Log in</button>
                     </div>
                 </form>
             </div>
